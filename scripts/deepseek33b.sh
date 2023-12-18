adf8ba16d5f0b52c0313a6cc4eebdc24cd294b06
Brian Williams <brian@vartia.ai>
2023-12-18 12:05:42 -0800

    added more env flags to deepseek

--- scripts/deepseek33b.sh
+++ scripts/deepseek33b.sh
@@ -2,7 +2,8 @@
 export NCCL_P2P_DISABLE=1
 export TOKENIZERS_PARALLELISM=true
 export CUDA_VISIBLE_DEVICES=0,1
+export ENABLE_INTRA_NODE_COMM=1
+export OMP_NUM_THREADS=16
 export MODEL_REPO=deepseek-ai/deepseek-coder-33b-instruct
 export DRAFT_MODEL_REPO=deepseek-ai/deepseek-coder-1.3b-instruct
-time torchrun --nproc_per_node=2 generate.py --compile  --compile_prefill --draft_checkpoint_path checkpoints/$DRAFT_MODEL_REPO/model_int8.pth  --checkpoint_path checkpoints/$MODEL_REPO/model_int4.g32.pth --speculate_k 6 --prompt "def quicksort(arr):" --max_new_tokens 16000 --num_samples 10
-
+time torchrun  --standalone --nproc_per_node=2 generate.py --compile  --compile_prefill --draft_checkpoint_path checkpoints/$DRAFT_MODEL_REPO/model_int8.pth  --checkpoint_path checkpoints/$MODEL_REPO/model_int4.g32.pth --speculate_k 6 --prompt "def quicksort(arr):" --max_new_tokens 16000 --num_samples 10
