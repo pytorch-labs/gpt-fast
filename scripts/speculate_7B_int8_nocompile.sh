dd30043ea1bb1444860af0e2a73194932c7f737b
yafehlis <yao.fehlis@amd.com>
2024-01-09 20:28:30 +0000

    no compile runs fast

---
+++ scripts/speculate_7B_int8_nocompile.sh
@@ -0,0 +1,3 @@
+export MODEL_REPO=codellama/CodeLlama-7b-Python-hf
+export DRAFT_MODEL_REPO=PY007/TinyLlama-1.1B-intermediate-step-480k-1T
+time python generate.py --draft_checkpoint_path checkpoints/$DRAFT_MODEL_REPO/model_int8.pth  --checkpoint_path checkpoints/$MODEL_REPO/model_int8.pth --speculate_k 4 --prompt "Hi my name is" --max_new_tokens 10 --num_samples 1  --temperature 0.5 --prompt_file True
