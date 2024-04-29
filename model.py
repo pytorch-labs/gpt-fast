30d69b3245a29823e7c4c5ae6a1f48fa38267afd
Artem Bolgar <artem@sesameai.com>
2024-04-29 14:02:52 -0700

    llama3 8B support, tiktoken tokenizer (#158)
    * WIP: llama3 support, tiktoken tokenizer
    
    * Finalizing

--- model.py
+++ model.py
@@ -65,6 +65,7 @@ transformer_configs = {
     "Mistral-7B": dict(n_layer=32, n_head=32, n_local_heads=8, dim=4096, intermediate_size=14336, vocab_size=32000),
     "stories15M": dict(n_layer=6, n_head=6, dim=288),
     "stories110M": dict(n_layer=12, n_head=12, dim=768),
+    "Llama-3-8B": dict(block_size=8192, n_layer=32, n_head=32, n_local_heads=8, dim=4096, intermediate_size=14336, vocab_size=128256),
 }
 
 class KVCache(nn.Module):
