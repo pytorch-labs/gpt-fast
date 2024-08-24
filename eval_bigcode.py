94e2d2a54b3fefdc610f08ab9153dcdaeda65284
Mostafa Elhoushi <melhoushi@meta.com>
2024-08-24 22:20:41 +0000

    add option to provide stop words

--- eval_bigcode.py
+++ eval_bigcode.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Your CLI description.')
 
     parser.add_argument('--task', type=str, default="human_eval", help='Name of task')
-    parser.add_argument('--interactive', action='store_true', help='Whether to launch in interactive mode')
+    parser.add_argument('--no_stop_words', action='store_true', help='Do not stop words during generation')
     parser.add_argument('--num_samples', type=int, default=None, help='Number of samples.')
     parser.add_argument('--max_new_tokens', type=int, default=200, help='Maximum number of new tokens.')
     parser.add_argument('--top_k', type=int, default=None, help='Top-k for sampling.')
@@ -44,11 +44,11 @@ if __name__ == '__main__':
     num_samples = len(dataset) if args.num_samples is None else args.num_samples
     prompts = [task.get_prompt(dataset[i]) for i in range(num_samples)]
     references = [task.get_reference(dataset[i]) for i in range(num_samples)]
-    stop_words = task.stop_words
+    stop_words = task.stop_words if not args.no_stop_words else None
 
     # Generate text
     aggregate_metrics, generations = generate_samples(
-        prompts, args.interactive, num_samples, args.max_new_tokens, args.top_k, args.top_p,
+        prompts, False, num_samples, args.max_new_tokens, args.top_k, args.top_p,
         args.temperature, args.checkpoint_path, args.compile, args.compile_prefill, args.profile, args.draft_checkpoint_path, args.draft_early_exit,
         args.speculate_k, args.self_speculative, args.early_exit, args.device, args.log_results, args.log_generations, args.model_name, stop_words, args.max_seq_len
     )
