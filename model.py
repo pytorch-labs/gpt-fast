d497bcfa2069137b3a031b9a6b2340864fee7d12
Mostafa Elhoushi <melhoushi@meta.com>
2024-08-25 04:23:16 +0000

    get CodeLlama 34B to work

--- model.py
+++ model.py
@@ -57,6 +57,7 @@ class ModelArgs:
 
 transformer_configs = {
     "CodeLlama-7b-Python-hf": dict(block_size=16384, vocab_size=32000, n_layer=32, dim = 4096, rope_base=1000000),
+    "CodeLlama-7B": dict(n_layer=32, n_head=32, dim=4096, vocab_size=32016),
     "7B": dict(n_layer=32, n_head=32, dim=4096),
     "13B": dict(n_layer=40, n_head=40, dim=5120),
     "30B": dict(n_layer=60, n_head=52, dim=6656),
