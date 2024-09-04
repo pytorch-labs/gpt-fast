3f346d6d1d9f0c7d0d70b206736dccf5ce025eb4
Mostafa Elhoushi <melhoushi@meta.com>
2024-09-04 21:40:00 +0000

    add cli arguments to enable fsdp and/or mem_efficient_attn

--- sweep.py
+++ sweep.py
@@ -33,6 +33,8 @@ if __name__ == '__main__':
     parser.add_argument('--model_name', type=str, default=None, help='Model name to help find the architecture of the model.')
     parser.add_argument('--compile', action='store_true', help='Whether to compile the model.')
     parser.add_argument('--compile_prefill', action='store_true', help='Whether to compile the prefill (improves prefill perf, but higher compile times)')
+    parser.add_argument('--enable_flash', action='store_true', help='Whether to enable flash attention')
+    parser.add_argument('--enable_mem_efficient', action='store_true', help='Whether to enable memory efficient attention')
     parser.add_argument('--profile', type=Path, default=None, help='Profile path.')
     parser.add_argument('--speculate_k_start', type=int, default=5, help='Speculative execution depth start.')
     parser.add_argument('--speculate_k_end', type=int, default=5, help='Speculative execution depth end.')
@@ -64,13 +66,13 @@ if __name__ == '__main__':
             if args.dataset:
                 subprocess.check_call(
                     shlex.split(
-                        f"python benchmark.py --dataset={args.dataset} --n_shot={args.n_shot} {'--interactive' if args.interactive else ''} {'--num_samples='+str(args.num_samples) if args.num_samples else ''} --max_new_tokens={args.max_new_tokens} {'--top_k='+str(args.top_k) if args.top_k else ''} --top_p={args.top_p} --temperature={args.temperature} --checkpoint_path={args.checkpoint_path} {'--model_name='+str(args.model_name) if args.model_name else ''} {'--compile' if args.compile else ''} {'--compile_prefill' if args.compile_prefill else ''} {'--profile' if args.profile else ''} {'--draft_checkpoint_path='+str(args.draft_checkpoint_path) if args.draft_checkpoint_path else ''} --early_exit={early_exit} --speculate_k={speculate_k} {'--self_speculative' if args.self_speculative else ''} --device={args.device} --log_results={log_results}"
+                        f"python benchmark.py --dataset={args.dataset} --n_shot={args.n_shot} {'--interactive' if args.interactive else ''} {'--num_samples='+str(args.num_samples) if args.num_samples else ''} --max_new_tokens={args.max_new_tokens} {'--top_k='+str(args.top_k) if args.top_k else ''} --top_p={args.top_p} --temperature={args.temperature} --checkpoint_path={args.checkpoint_path} {'--model_name='+str(args.model_name) if args.model_name else ''} {'--compile' if args.compile else ''} {'--compile_prefill' if args.compile_prefill else ''} {'--enable_flash' if args.enable_flash else ''} {'--enable_mem_efficient' if args.enable_mem_efficient else ''} {'--profile' if args.profile else ''} {'--draft_checkpoint_path='+str(args.draft_checkpoint_path) if args.draft_checkpoint_path else ''} --early_exit={early_exit} --speculate_k={speculate_k} {'--self_speculative' if args.self_speculative else ''} --device={args.device} --log_results={log_results}"
                     )
                 )
             else:
                 subprocess.check_call(
                     shlex.split(
-                        f"python generate.py --prompt=\"{args.prompt}\" {'--interactive' if args.interactive else ''} {'--num_samples='+str(args.num_samples) if args.num_samples else ''} --max_new_tokens={args.max_new_tokens} {'--top_k='+str(args.top_k) if args.top_k else ''} --top_p={args.top_p} --temperature={args.temperature} --checkpoint_path={args.checkpoint_path} {'--model_name='+str(args.model_name) if args.model_name else ''} {'--compile' if args.compile else ''} {'--compile_prefill' if args.compile_prefill else ''} {'--profile' if args.profile else ''} {'--draft_checkpoint_path='+str(args.draft_checkpoint_path) if args.draft_checkpoint_path else ''} --early_exit={early_exit} --speculate_k={speculate_k} {'--self_speculative' if args.self_speculative else ''} --device={args.device} --log_results={log_results}"
+                        f"python generate.py --prompt=\"{args.prompt}\" {'--interactive' if args.interactive else ''} {'--num_samples='+str(args.num_samples) if args.num_samples else ''} --max_new_tokens={args.max_new_tokens} {'--top_k='+str(args.top_k) if args.top_k else ''} --top_p={args.top_p} --temperature={args.temperature} --checkpoint_path={args.checkpoint_path} {'--model_name='+str(args.model_name) if args.model_name else ''} {'--compile' if args.compile else ''} {'--compile_prefill' if args.compile_prefill else ''} {'--enable_flash' if args.enable_flash else ''} {'--enable_mem_efficient' if args.enable_mem_efficient else ''} {'--profile' if args.profile else ''} {'--draft_checkpoint_path='+str(args.draft_checkpoint_path) if args.draft_checkpoint_path else ''} --early_exit={early_exit} --speculate_k={speculate_k} {'--self_speculative' if args.self_speculative else ''} --device={args.device} --log_results={log_results}"
                     )
                 )
 
