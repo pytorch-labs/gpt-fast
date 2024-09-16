32971d3129541c5bfb4f715abc33d1c5f408d204
Yanbo Liang <ybliang8@gmail.com>
2024-09-15 22:21:37 -0700

    Fix Llama3 HF checkpoint converter (#202)

--- scripts/convert_hf_checkpoint.py
+++ scripts/convert_hf_checkpoint.py
@@ -115,7 +115,7 @@ def convert_hf_checkpoint(
             del final_result[key.replace("wq", "wv")]
     print(f"Saving checkpoint to {checkpoint_dir / 'model.pth'}")
     torch.save(final_result, checkpoint_dir / "model.pth")
-    if 'llama-3' in model_name.lower():
+    if 'llama-3-' in model_name.lower() or 'llama-3.1-' in model_name.lower():
         if 'llama-3.1-405b' in model_name.lower():
             original_dir = checkpoint_dir / "original" / "mp16"
         else:
